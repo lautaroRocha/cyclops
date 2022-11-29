492b6cb2a7c34f55fd1e57acf8163ab7c30c9cda
Lautaro Rocha <lautarovrocha@gmail.com>
2022-11-29 11:55:44 -0300

    added errorHandler for app

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000
 const router = require('./routes/routes');
 const cors = require('cors')
+const errorHandler = require('./middleware/errorHandler')
 require('./config/mongoConnection');
 
 
@@ -10,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(router)
-
+app.use(errorHandler)
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     }
