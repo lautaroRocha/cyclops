8172a0a3e59576a37ee86580db300c6649661c2c
Lautaro Rocha <lautarovrocha@gmail.com>
2022-11-29 14:39:07 -0300

    set up for multer

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(router)
+app.use('/upload', express.static('upload'));
 app.use(errorHandler)
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
