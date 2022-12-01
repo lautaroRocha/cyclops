a11ebf101a3311c0554beee7f27d05fe2a8987f9
Lautaro Rocha <lautarovrocha@gmail.com>
2022-12-01 13:50:02 -0300

    encrypted password

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,9 +1,11 @@
 const Admin = require('../models/admin');
+const bcrypt = require('bcrypt');
+
 
 async function addAdmin(req, res){
     const adminData = {
         email: req.body.email,
-        password : req.body.password
+        password : bcrypt.hashSync(req.body.password, 10)
         }
     const admin = new Admin(adminData);
     await admin.save();
@@ -13,10 +15,16 @@ async function addAdmin(req, res){
 async function logInUser(req, res){
     const {email, password} = req.body
     const tryingUser = await Admin.findOne({email : email}).exec()
-    if(tryingUser.password !== password){
-        res.status(401).json({message : "credenciales inválidas"})
+    if(!tryingUser){
+        res.status(404).json({message : "No hay ningun usuario con ese email"})
     }else{
-        res.json({message: "listo para loguear"})
+        const validPass = bcrypt.compareSync(password, tryingUser.password)
+        if(!validPass){
+            res.status(401).json({message : "credenciales inválidas"})
+        }else{
+            res.json({message: "listo para loguear"})
+        }
     }
+   
 }   
 module.exports = {addAdmin, logInUser};
